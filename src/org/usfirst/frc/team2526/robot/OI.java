e4a36a249b26ffea40a8953e5e018546b2079b00
Alex Kafer <alexkafer@gmail.com>
2015-02-09 16:17:06 -0600

    Added Joystick commands

--- src/org/usfirst/frc/team2526/robot/OI.java
+++ src/org/usfirst/frc/team2526/robot/OI.java
@@ -1,6 +1,10 @@
 package org.usfirst.frc.team2526.robot;
 
+import org.usfirst.frc.team2526.robot.commands.StartAlign;
+
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -19,31 +23,10 @@ public class OI {
 		return secondaryStick;
 	}
 	
-    //// CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-    // Joystick stick = new Joystick(port);
-    // Button button = new JoystickButton(stick, buttonNumber);
-    
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
-    
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-    
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new ExampleCommand());
-    
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
-    
-    // Start the command when the button is released  and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new ExampleCommand());
+	Button primaryTriggerStick = new JoystickButton(primaryStick, 1);
+	
+	public OI() {
+		primaryTriggerStick.whileHeld(new StartAlign());
+	}
 }
 
--- src/org/usfirst/frc/team2526/robot/commands/StartAlign.java
+++ src/org/usfirst/frc/team2526/robot/commands/StartAlign.java
@@ -21,7 +21,7 @@ public class StartAlign extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {}
 
-    // Make this return true when this Command no longer needs to run execute()
+    // Make this return true when this Comma  nd no longer needs to run execute()
     protected boolean isFinished() {
         return false;
     }
