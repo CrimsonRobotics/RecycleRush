bcb8dca6a504fd3fb3978afdebb6d4dea4bfd452
alexkafer <alexkafer@gmail.com>
2015-02-13 17:31:41 -0600

    Eh, forgot a few things

--- src/org/usfirst/frc/team2526/robot/subsystems/Elevator.java
+++ src/org/usfirst/frc/team2526/robot/subsystems/Elevator.java
@@ -3,12 +3,8 @@ package org.usfirst.frc.team2526.robot.subsystems;
 import org.usfirst.frc.team2526.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.CANTalon;
-<<<<<<< HEAD
-import edu.wpi.first.wpilibj.command.Subsystem;
-=======
 import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.command.PIDSubsystem;
->>>>>>> branch 'master' of git@github.com:CrimsonRobotics/RecycleRush.git
+import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
@@ -25,14 +21,10 @@ public class Elevator extends Subsystem/*extends PIDSubsystem*/ {
 	public Solenoid solenoidBrake;
 	
     public Elevator() {
-<<<<<<< HEAD
     	//super("Elevator", 1, 1, 1);
     	super("Elevator");
-    	
-=======
-    	super("Elevator", 1, 1, 1);
     	solenoidBrake = new Solenoid(RobotMap.PCM_MAIN, RobotMap.WINCH_BRAKE);
->>>>>>> branch 'master' of git@github.com:CrimsonRobotics/RecycleRush.git
+    	
     	winchA = new CANTalon(RobotMap.WINCH_A_TALON);
     	winchB = new CANTalon(RobotMap.WINCH_B_TALON);
     	
