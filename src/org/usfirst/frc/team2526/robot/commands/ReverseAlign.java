15b1140c33e9ccf9a360d1dace168ff72f04f531
alexkafer <alexkafer@gmail.com>
2015-02-12 18:44:12 -0600

    Adding reverse align

---
+++ src/org/usfirst/frc/team2526/robot/commands/ReverseAlign.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team2526.robot.commands;
+
+import org.usfirst.frc.team2526.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ReverseAlign extends Command {
+
+    public ReverseAlign() {
+        // Use requires() here to declare subsystem dependencies
+        requires(Robot.alignment);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.alignment.reverseAlign();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.alignment.stopAlign();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
